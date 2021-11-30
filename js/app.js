4a0f3a33e417cc4055f3826fa13b9b981871f789
Michael McCarthy <mike@pixelonezero.com>
2021-11-30 10:23:18 -0800

    Add task for caching data in local storage. Add immediately-invoked function for app initialization. Fix indenting (4 spaces instead of 2)

--- js/app.js
+++ js/app.js
@@ -2,46 +2,49 @@
 
 Tasks:
 - Remove Angular dependencies
-- Use fetch for asynchronous requests
 - Remove jquery dependencies
+- Add local storage caching
+- Use fetch for asynchronous requests
 - Add form control to sort stories by:
 		- new
 		- top
 		- best
 */
 
-var app = {
-	"name": "HackerNewsNow",
-	"version": "0.1",
-	"endpoints": {
-		"topStories": "https://hacker-news.firebaseio.com/v0/topstories.json",
-		"story": "https://hacker-news.firebaseio.com/v0/item/"
-	},
-	init: function() {
-		var storyUri;
+(function () {
+		var app = {
+				"name": "HackerNewsNow",
+				"version": "0.1",
+				"endpoints": {
+						"topStories": "https://hacker-news.firebaseio.com/v0/topstories.json",
+						"story": "https://hacker-news.firebaseio.com/v0/item/"
+				},
+				init: function() {
+						var storyUri;
 
-		$.getJSON( app.endpoints.topStories, function(data) {
-			$.each(data, function(i) {
-				storyUri = app.endpoints.story + data[i] + ".json";
-				app.getStory(storyUri);
-			})
-		});
-	},
-	getStory: function(uri) {
-		$.getJSON( uri, function(data) {
-			headline = "<a href='" + data.url + "' target='_blank'>" + data.title + "</a>";
-			headline += "<small>, score: " + data.score + ", posted " + app.convertUnixTime(data.time) + "</small>";
-			app.writeHeadline(headline);
-		});
-	},
-	writeHeadline: function(title) {
-		var headlinesList = $('[data-container=headlines]');
-		headlinesList.append('<li>' + title + '</li>');
-	},
-	convertUnixTime: function(unixTime) {
-		var date = new Date(unixTime * 1000);
-		return date;
-	}
-}
+						$.getJSON( app.endpoints.topStories, function(data) {
+								$.each(data, function(i) {
+										storyUri = app.endpoints.story + data[i] + ".json";
+										app.getStory(storyUri);
+								})
+						});
+				},
+				getStory: function(uri) {
+						$.getJSON( uri, function(data) {
+								headline = "<a href='" + data.url + "' target='_blank'>" + data.title + "</a>";
+								headline += "<small>, score: " + data.score + ", posted " + app.convertUnixTime(data.time) + "</small>";
+								app.writeHeadline(headline);
+						});
+				},
+				writeHeadline: function(title) {
+						var headlinesList = $('[data-container=headlines]');
+						headlinesList.append('<li>' + title + '</li>');
+				},
+				convertUnixTime: function(unixTime) {
+						var date = new Date(unixTime * 1000);
+						return date;
+				}
+		}
 
-document.onload = app.init();
+		document.onload = app.init();
+})();
