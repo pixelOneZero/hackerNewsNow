4ec0a6bf2a10dde12811124f20ab320af4119a4b
Michael McCarthy <mike@pixelonezero.com>
2021-12-03 11:27:44 -0800

    Reduce whitespace indentation from 4 to 2 spaces throughout app.js

--- js/app.js
+++ js/app.js
@@ -12,52 +12,53 @@ done - Use fetch for asynchronous requests
 */
 
 (() => {
-		const app = {
-				"name": "HackerNewsNow",
-				"version": "0.1.1",
-				"endpoints": {
-						"topStories": "https://hacker-news.firebaseio.com/v0/topstories.json",
-						"story": "https://hacker-news.firebaseio.com/v0/item/"
-				},
-				init: () => {
-						let storyUri;
+	const app = {
+		"name": "HackerNewsNow",
+		"version": "0.1.1",
+		"endpoints": {
+			"topStories": "https://hacker-news.firebaseio.com/v0/topstories.json",
+			"story": "https://hacker-news.firebaseio.com/v0/item/"
+		},
+		init: () => {
+			let storyUri;
 
-						fetch(app.endpoints.topStories)
-						  .then(response => response.json())
-							.then(data => {
-								for (story in data) {
-									storyUri = `${app.endpoints.story}${story}.json`;
-									app.getStory(storyUri);
-								}
-							})
-							.catch(error => {
-						    console.error('Fetch error: ', error);
-						  });
-				},
-				getStory: (uri) => {
-					fetch(uri)
-						.then(response => response.json())
-						.then(data => {
-								if (data?.title) {
-									headline = `<a href='${data.url}' target='_blank'>${data.title}</a>
-										<small>, score: ${data.score}, posted ${app.convertUnixTime(data.time)}</small>`;
-									app.writeHeadline(headline);
-								}
-						})
-						.catch(error => {
-							console.error('Fetch error: ', error);
-						});
-				},
-				writeHeadline: (title) => {
-						const headlinesList = document.querySelector('[data-container=headlines]');
-						if (headlinesList) {
-							headlinesList.innerHTML += `<li>${title}</li>`;
-						}
-				},
-				convertUnixTime: (unixTime) => {
-						return new Date(unixTime * 1000);
-				}
+			fetch(app.endpoints.topStories)
+			  .then(response => response.json())
+				.then(data => {
+					for (story in data) {
+						storyUri = `${app.endpoints.story}${story}.json`;
+						app.getStory(storyUri);
+					}
+				})
+				.catch(error => {
+			    console.error('Fetch error: ', error);
+			  });
+		},
+		getStory: (uri) => {
+			fetch(uri)
+				.then(response => response.json())
+				.then(data => {
+					if (data?.title) {
+						headline = `<a href='${data.url}' target='_blank'>${data.title}</a>
+							<small>, score: ${data.score}, posted ${app.convertUnixTime(data.time)}</small>`;
+						app.writeHeadline(headline);
+					}
+				})
+				.catch(error => {
+					console.error('Fetch error: ', error);
+				});
+		},
+		writeHeadline: (title) => {
+			const headlinesList = document.querySelector('[data-container=headlines]');
+			if (headlinesList) {
+				headlinesList.innerHTML += `<li>${title}</li>`;
+			}
+		},
+		convertUnixTime: (unixTime) => {
+			return new Date(unixTime * 1000);
 		}
+	}
 
-		document.onload = app.init();
+	document.onload = app.init();
+
 })();
